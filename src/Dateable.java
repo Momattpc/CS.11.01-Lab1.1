2453ec23cc54824094270614abd38854802732f4
moses <moseskplai@gmail.com>
2023-08-31 14:40:45 +0800

    Added Lab 1 solution

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,15 @@
-public class Dateable {
+import java.util.Scanner;
 
+public class Dateable {
     public static void main(String[] args) {
-        
+      System.out.println("(please note that all odd numbers are rounded DOWN)");
+      Scanner scanner1 = new Scanner(System.in);
+      System.out.println("What is your age? (No trolling please)");
+      int userAge = scanner1.nextInt();
+
+      int daterAge = (userAge / 2);
+      int daterAgeRounded = (Math.round(daterAge) + 7);
+      System.out.println( userAge + "-year olds should date someone who is at least" + " " + daterAgeRounded + " " + "years old");
+
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,19 @@
-public class Pages {
+import java.util.Scanner;
 
+public class Pages {
     public static void main(String[] args) {
+        System.out.println("(please note that all odd numbers are rounded DOWN");
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.println("What is your age? (No trolling please)");
+        int userAge = scanner1.nextInt();
+    if (userAge > 100)
+        System.out.println("If you're over 100, go enjoy life while it still lasts!");
+    else if (userAge == 100)
+        System.out.println("Congrats! Instead of reading, you can work on writing your will!");
+    else if (userAge < 100) {
+        int pagesRead = (100 - userAge);
+        System.out.println( "As you are " + userAge + "," + " " + "you should read at least " + pagesRead + " pages before giving up on a book (and going to sleep) .");
+    }
 
     }
 }
